8b6227c275834124e94aa843d2997988f6fb1cb3
r3X <rch246chauhan@gmail.com>
2022-11-15 13:57:06 +0530

    CRUD and queries

--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -7,8 +7,8 @@ const mongoose = require('mongoose')
 
 const router = express.Router()
 
-router.post("/:id", async (req, res) => {
-    const newReview = new Post(req.body)
+router.post("/", async (req, res) => {
+    const newReview = new Review(req.body)
     try {
 
 
@@ -20,41 +20,74 @@ router.post("/:id", async (req, res) => {
         res.status(500).json(err)
     }
 })
-router.delete("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id) {
-        try {
-            const user = await User.findById(req.params.id)
-            if (!user) {
-                res.status(404).send("User not found")
-            }
+router.put("/:id", async (req, res) => {
+    try {
+        const review = await Review.findByIdAndUpdate(req.params.id)
 
+        if (req.body.author === review.author) {
             try {
-                await Review.deleteMany({ username: user.username })
-                await User.findByIdAndDelete(req.params.id)
-                res.status(200).json({ message: "User deleted" })
+                const updatedREview = await Post.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+                res.status(200).json("Review updated")
             } catch (err) {
-                console.log(err);
                 res.status(500).json(err)
             }
-        } catch (error) {
-
-
-
+        } else {
+            res.status(401).json("Not authenticated")
         }
-    } else {
-        res.status(401).json("Not authenticated")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
+router.delete("/:id", async (req, res) => {
+    const review = await Review.findById(req.params.id)
+
+    try {
+        if (req.body.author === review.author) {
+            try {
+                await review.delete()
+                res.status(200).json("Review deleted")
+            } catch (err) {
+                res.status(500).json(err)
+
+            }
+        } else {
+
+            res.status(401).json("Not authenticated")
+        }
+    } catch (error) {
+        res.status(500).json(error)
     }
 })
 
 router.get("/:id", async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
 
-        const { password, otherDetails } = user._doc
-        res.status(200).json(otherDetails)
-    } catch (error) {
-        res.status(500).json(error)
+    try {
+        const review = await Review.findById(req.params.id)
+
+        res.status(200).json(review)
+    } catch (err) {
+
+        res.status(500).json(err)
+    }
+})
+router.get("/", async (req, res) => {
+    const author = req.query.author;
+    const category = req.query.cat
+    try {
+        let reviews
+        if (author) {
+            reviews = await Review.find({ author })
+        } else if (category) {
+            reviews = await Review.find({ categories: { $in: [category] } })
+        } else {
+            reviews = await Review.find()
+        }
+
+        res.status(200).json(reviews)
+    } catch (err) {
+
+        res.status(500).json(err)
     }
 })
 
