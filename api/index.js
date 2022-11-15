e7bbbe05e301f7843d524798d904d15b0c9e1bd4
r3X <rch246chauhan@gmail.com>
2022-11-15 19:08:27 +0530

    multer and category route added

--- api/index.js
+++ api/index.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const multer = require('multer')
 
 const authRoute = require('./routes/auth')
-const userRoute=require('./routes/users')
-const reviewRoute=require("./routes/reviews")
+const userRoute = require('./routes/users')
+const reviewRoute = require("./routes/reviews")
+const categoryRoute = require("./routes/catergories")
+
 
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => (console.log("DB connected"))).catch(err => (console.log(err)))
 
+const storage = multer({ destination: (req, file, cb) => { cb(null, "images") }, filename: (req, file, cb) => { cb(null, req.body.name) } })
+
+const upload = multer({ storage: storage })
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("Image has been uploaded")
+})
+
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/reviews", reviewRoute)
+app.use("/api/categories", categoryRoute)
 
 app.listen(8080, () => {
     console.log("Server is up and running at http://localhost:8080");
--- api/routes/catergories.js
+++ api/routes/catergories.js
@@ -1,1 +1,32 @@
 const express=require('express')
+const Category=require('../models/Category')
+
+
+const router=express.Router()
+
+router.post("/",async(req,res)=>{
+    const categories=await Ca(req.body)
+
+    try {
+        const savedCategories=await categories.save()
+
+        res.status(200).json(savedCategories)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+router.get("/",async(req,res)=>{
+
+
+    try {
+        const categories=await Category.find()
+        res.status(200).json(categories)
+    } catch (error) {
+        res.status(200).json(err)
+    }
+})
+
+
+module.exports=router
